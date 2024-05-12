c83fd40021c6f64f1404eb90cb5b77c4e8ad07e3
Nikolay <m.nikolay1990@gmail.com>
2024-05-12 18:59:06 +0700

    add ngModel lesson

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,12 +4,29 @@ import { Component } from '@angular/core';
   selector: 'app-servers',
   // selector: '[app-servers]',
   // selector: '.app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // `,
+  templateUrl: './servers.component.html',
   styleUrl: './servers.component.css'
 })
 export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
 
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
+
+  // onUpdateServeName(event: Event) {
+  //   this.serverName = event.target['value'];
+  // }
 }
